7f95e81275582b1a1380bd2a457603827a1c4777
Marlon Frausto <marlonjfrausto@gmail.com>
2020-06-23 13:37:27 -0400

    blagh.

--- src/browser.tsx
+++ src/browser.tsx
@@ -2,7 +2,7 @@ import { React, ReactDOM } from "https://unpkg.com/es-react";
 import { App } from "./http/get-index/app.tsx";
 
 window.addEventListener("DOMContentLoaded", () => {
-
+  //@ts-ignore
   let el = window.document.getElementById("app");
   //@ts-ignore
   ReactDOM.hydrate(<App />, el);
