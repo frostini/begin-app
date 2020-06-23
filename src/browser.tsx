e75308433275e9e9e4536616cc6266a272e03786
Marlon Frausto <marlonjfrausto@gmail.com>
2020-06-23 13:36:06 -0400

    moved ts ignore to target browser.tsx  instantiation of the rehydrated App component.

--- src/browser.tsx
+++ src/browser.tsx
@@ -2,7 +2,8 @@ import { React, ReactDOM } from "https://unpkg.com/es-react";
 import { App } from "./http/get-index/app.tsx";
 
 window.addEventListener("DOMContentLoaded", () => {
-  //@ts-ignore
+
   let el = window.document.getElementById("app");
+  //@ts-ignore
   ReactDOM.hydrate(<App />, el);
 });
